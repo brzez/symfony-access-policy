e47c01d0dc3bd2908b0f2a0e89417feab37ad731
brzez <pawel@brzez.com>
2016-05-20 10:05:20 +0200

    ContainerAwareAccessPolicyProvider; Registered it as a service; AccessCheckerTrait - can be used to add can() & cannot() methods to controllers; Configuration - basic validation for policies in config

---
+++ Service/ContainerAware/ContainerAwareAccessPolicyProvider.php
@@ -0,0 +1,17 @@
+<?php 
+namespace Brzez\AccessPolicyBundle\Service\ContainerAware;
+
+use Brzez\AccessPolicyBundle\Service\AccessPolicy;
+use Brzez\AccessPolicyBundle\Service\AccessPolicyProvider;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ContainerAwareAccessPolicyProvider extends AccessPolicyProvider implements ContainerAwareInterface
+{
+    protected $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+}
