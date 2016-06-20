a5b5c16cba188ba078c362c949ea82e487cd2c7c
brzez <pawel@brzez.com>
2016-06-20 19:52:11 +0200

    registering tagged services (with policied class from tag data)

--- PolicyCompilerPass.php
+++ PolicyCompilerPass.php
@@ -3,6 +3,8 @@ namespace Brzez\AccessPolicyBundle;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
 class PolicyCompilerPass implements CompilerPassInterface
 {
@@ -15,12 +17,27 @@ class PolicyCompilerPass implements CompilerPassInterface
             return;
         }
 
-        $policyProvider = $containerBuilder->get(self::POLICY_PROVIDER_SERVICE);
+        $providerDefinition = $containerBuilder->findDefinition(self::POLICY_PROVIDER_SERVICE);
 
         $policies = $containerBuilder->findTaggedServiceIds(self::POLICY_SERVICE_TAG);
 
         foreach($policies as $id => $tags){
-            var_dump($id, $tags);
+            $this->registerPolicy($providerDefinition, $id, $tags);
+        }
+    }
+
+    public function registerPolicy(Definition $providerDefinition, $id, $tags)
+    {
+        foreach ($tags as $tag) {
+            if(empty($tag['class'])){
+                throw new \UnexpectedValueException("Missing argument `class` for policy: $id");
+            }
+            $class = $tag['class'];
+
+            $providerDefinition->addMethodCall('registerPolicy', [
+                $class, 
+                new Reference($id)
+            ]);
         }
     }
 }
