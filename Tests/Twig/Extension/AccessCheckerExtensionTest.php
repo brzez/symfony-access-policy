1e59af1814826373dec36b2fc34c7cbe251218ea
brzez <pawel@brzez.com>
2016-07-02 21:13:21 +0200

    Fixed tests

--- Tests/Twig/Extension/AccessCheckerExtensionTest.php
+++ Tests/Twig/Extension/AccessCheckerExtensionTest.php
@@ -2,8 +2,8 @@
 
 namespace Brzez\AccessPolicyBundle\Tests\Twig\Extension;
 
-use Brzez\AccessPolicyBundle\Service\ContainerAware\ContainerAwareAccessPolicyProvider;
-use Brzez\AccessPolicyBundle\Tests\Service\Mocks\Apple;
+use Brzez\AccessPolicyBundle\Service\AccessPolicyProvider;
+use Brzez\AccessPolicyBundle\Tests\Mocks\Apple;
 use Brzez\AccessPolicyBundle\Twig\Extension\AccessCheckerExtension;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -13,7 +13,9 @@ class AccessCheckerExtensionTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function it_passes_multiple_args_to_the_policyprovider()
     {
-        $providerMock = $this->getMock(ContainerAwareAccessPolicyProvider::class);
+        $providerMock = $this->getMockBuilder(AccessPolicyProvider::class)
+            ->disableOriginalConstructor()
+            ->getMock();
 
         $providerMock->expects($this->once())->method('can')
             ->with('intent', $this->isInstanceOf(Apple::class), 'arg 1', 'arg 2')
