632755d4762fcc01affebe325260a16925e73585
brzez <pawel@brzez.com>
2016-07-13 08:09:14 +0200

    initial test for this issue

--- Tests/Service/AccessPolicyProviderTest.php
+++ Tests/Service/AccessPolicyProviderTest.php
@@ -4,6 +4,7 @@ namespace Brzez\AccessPolicyBundle\Tests\Service;
 use Brzez\AccessPolicyBundle\Service\AccessPolicy;
 use Brzez\AccessPolicyBundle\Service\AccessPolicyProvider;
 use Brzez\AccessPolicyBundle\Service\AccessPolicyResolver;
+use Brzez\AccessPolicyBundle\Tests\Mocks\AnotherApplePolicy;
 use Brzez\AccessPolicyBundle\Tests\Mocks\Apple;
 use Brzez\AccessPolicyBundle\Tests\Mocks\ApplePolicy;
 use Brzez\AccessPolicyBundle\Tests\Mocks\Orange;
@@ -12,16 +13,19 @@ use PHPUnit_Framework_TestCase;
 class AccessPolicyProviderTest extends PHPUnit_Framework_TestCase
 {
     /** @test */
-    public function throws_when_registering_multiple_policies_for_the_same_class()
+    public function supports_multiple_policies()
     {
-        $this->setExpectedException(\Exception::class);
-        
         $resolver = $this->getMockBuilder(AccessPolicyResolver::class)->disableOriginalConstructor()->getMock();
 
         $provider = new AccessPolicyProvider($resolver);
 
         $provider->registerPolicy(new ApplePolicy);
-        $provider->registerPolicy(new ApplePolicy);
+        $provider->registerPolicy(new AnotherApplePolicy);
+
+        $apple = new Apple;
+
+        $this->assertTrue($provider->can('do-thing', $apple));
+        $this->assertFalse($provider->can('do-other-thing', $apple));
     }
 
     /** @test */
